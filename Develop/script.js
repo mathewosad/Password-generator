1b17eb1b55729683444aa1a4d24a9f3f6fc52a28
mathewosad <matthewosad@yahoo.com>
2022-01-21 00:27:17 -0800

    updated

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,21 @@
-// Assignment Code
+
 // Ask user for length of password
 // Ask user for type of password (lowercase, uppercase, numeric or special character)
-// 'a'-'z' = 97-122 in ACII
-// 'A'-'Z' = 65-90 in ACII
-// '0'-'9' = 48-57 in ACII
-// '33'-'47' = Special characters
 
 const empty = "";
+// 'A'-'Z' = 65-90 in ACII
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// 'a'-'z' = 97-122 in ACII
 const lower = "abcdefghijklmnopqrstuvwxyz"
+// '0'-'9' = 48-57 in ACII
 const number = "0123456789"
+//ACII '33'-'47' = Special characters
 const symbol = "&!@#$%^*(){}[]_|<>+="
 
 
 // Write password to the #password input
 
 //  Dom elements
-
 const length = document.getElementById('length');
 const uppercase = document.getElementById('uppercase');
 const lowercase = document.getElementById('lowercase');
@@ -26,7 +25,7 @@ const result = document.getElementById('result');
 const generate = document.getElementById('generate');
 
 // Generator Functions
-// Add event listener to generate button
+// Added event listener to generate button
 generate.addEventListener("click", () => {
   let initialPassword = "";
   (uppercase.checked) ? initialPassword += uppercase : "";
@@ -39,25 +38,12 @@ generate.addEventListener("click", () => {
 // Generate a random number that represents all possible characters in our password
 function generatePassword(l, initialPassword) {
   let pass = "";
-  // Added for loop
+  // Added for loop 10 posible digits + 26 possible uppercase letters + 26 possible lowercase letters = 62 possible characters
   for (let i = 0; i < l; i++){
     pass += initialPassword.charAt(Math.floor(Math.random() * initialPassword.length));
+
   }
-  console.log(pass);
+
   return pass;
   
 }
-
-
-
-
-// Ask user for length of password
-// Ask user for type of password (lowercase, uppercase, numeric or special character)
-
-// 'a'-'z' = 97-122 in ACII
-// 'A'-'Z' = 65-90 in ACII
-// '0'-'9' = 48-57 in ACII
-// '33'-'47' = Special characters
-
-
-// 10 posible digits + 26 possible uppercase letters + 26 possible lowercase letters = 62 possible characters
