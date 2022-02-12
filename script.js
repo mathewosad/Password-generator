8816c881f95c1f8e5a1030a0a86698d53693f4d9
mathewosad <96810252+mathewosad@users.noreply.github.com>
2022-02-12 13:40:01 -0800

    fixed my Js

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ var generateBtn = document.querySelector("#generate");
 
 const empty = "";
 // 'A'-'Z' = 65-90 in ACII
-const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 // 'a'-'z' = 97-122 in ACII
-const lower = "abcdefghijklmnopqrstuvwxyz"
+const lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 // '0'-'9' = 48-57 in ACII
-const numbers = "0123456789"
+const numbers = ["0","1","2","3","4","5","6","7","8","9"];
 //ACII '33'-'47' = Special characters
-const symbols = "&!@#$%^*(){}[]_|<>+="
+const symbols = ["&","!","@","#","$","%","^","*","(",")","{","}","[","]","_","|","<",">","+","="];
 
 
 // Write password to the #password input
